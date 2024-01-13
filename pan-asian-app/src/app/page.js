a54a3e34d05fd219a6d9378f09fdae82cee3e03b
Karish Gupta <karishgupta2003@gmail.com>
2024-01-13 00:39:57 -0500

    Second iteration of main page

--- pan-asian-app/src/app/page.js
+++ pan-asian-app/src/app/page.js
@@ -10,22 +10,147 @@ export default function Home() {
 
       </div>
 
+
       <div className="mb-10 lg:mb-32 w-full grid text-center">
-    <a
+    `<a
       href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
       className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
       target="_blank"
       rel="noopener noreferrer"
-    >
-      <h2 className={`mb-3 text-2xl font-semibold`}>
-        Deploy{' '}
-        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-          -&gt;
-        </span>
-      </h2>
+    >`
+    <h2 className={`mb-3 text-3xl font-semibold text-gray-700`}>
+      Society of Asian Scientists and Engineers{' '}
+      <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+        -&gt;
+      </span>
+    </h2>
 
     </a>
     </div>
+
+
+    <div className="mb-10 lg:mb-32 w-full grid text-center">
+    `<a
+      href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+      className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+      target="_blank"
+      rel="noopener noreferrer"
+    >`
+    <h2 className={`mb-3 text-3xl font-semibold text-gray-700`}>
+      South Asian Student Association{' '}
+      <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+        -&gt;
+      </span>
+    </h2>
+
+    </a>
+    </div>
+
+    <div className="mb-10 lg:mb-32 w-full grid text-center">
+    `<a
+      href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+      className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+      target="_blank"
+      rel="noopener noreferrer"
+    >`
+    <h2 className={`mb-3 text-3xl font-semibold text-gray-700`}>
+      Vietnamese Student Association{' '}
+      <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+        -&gt;
+      </span>
+    </h2>
+
+    </a>
+    </div>
+
+    <div className="mb-10 lg:mb-32 w-full grid text-center">
+    `<a
+      href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+      className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+      target="_blank"
+      rel="noopener noreferrer"
+    >`
+    <h2 className={`mb-3 text-3xl font-semibold text-gray-700`}>
+      Filipino Student Association{' '}
+      <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+        -&gt;
+      </span>
+    </h2>
+
+    </a>
+    </div>
+
+    <div className="mb-10 lg:mb-32 w-full grid text-center">
+    `<a
+      href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+      className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+      target="_blank"
+      rel="noopener noreferrer"
+    >`
+    <h2 className={`mb-3 text-3xl font-semibold text-gray-700`}>
+      Chinese Student and Scholar Association{' '}
+      <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+        -&gt;
+      </span>
+    </h2>
+
+    </a>
+    </div>
+
+
+    <div className="mb-10 lg:mb-32 w-full grid text-center">
+    `<a
+      href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+      className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+      target="_blank"
+      rel="noopener noreferrer"
+    >`
+    <h2 className={`mb-3 text-3xl font-semibold text-gray-700`}>
+       Korean Student Association{' '}
+      <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+        -&gt;
+      </span>
+    </h2>
+
+    </a>
+    </div>
+
+
+    <div className="mb-10 lg:mb-32 w-full grid text-center">
+    `<a
+      href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+      className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+      target="_blank"
+      rel="noopener noreferrer"
+    >`
+    <h2 className={`mb-3 text-3xl font-semibold text-gray-700`}>
+    Hawaiian Cultural Association{' '}
+      <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+        -&gt;
+      </span>
+    </h2>
+
+    </a>
+    </div>
+
+
+    <div className="mb-10 lg:mb-32 w-full grid text-center">
+    `<a
+      href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+      className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+      target="_blank"
+      rel="noopener noreferrer"
+    >`
+    <h2 className={`mb-3 text-3xl font-semibold text-gray-700`}>
+    Japanese Student Association{' '}
+      <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+        -&gt;
+      </span>
+    </h2>
+
+    </a>
+    </div>
+
     </main>
   )
 }
