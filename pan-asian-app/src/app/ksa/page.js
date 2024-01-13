28cef536826638bf15c0c7ae0919c618a8ff9d4a
Abbas Jivan <abbasjivan04@gmail.com>
2024-01-13 15:38:44 -0500

    commit

---
+++ pan-asian-app/src/app/ksa/page.js
@@ -0,0 +1,20 @@
+"use client"
+import React, { useState } from 'react';
+import NavBar from '../components/navbar';
+import ClubPage from '../components/clubpage';
+
+const ksaPage = () => {
+
+    return (
+        <body className="flex min-h-screen flex-col items-center justify-between p-24">
+            <header className="w-full bg-[#ebd4c7]">             
+                <NavBar></NavBar>
+            </header>
+            <ClubPage>               
+            </ClubPage>       
+        </body>   
+  ); 
+  };
+  
+  export default ksaPage;
+  
