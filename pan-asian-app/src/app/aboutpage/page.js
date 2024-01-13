c86b820f1ffd1dfd3550655da1d2940833df189b
NomblydoomPC <kurtzjeremy126@gmail.com>
2024-01-13 14:00:01 -0500

    aboutpage updated with nav

--- pan-asian-app/src/app/aboutpage/page.js
+++ pan-asian-app/src/app/aboutpage/page.js
@@ -6,18 +6,30 @@ const AboutPage = () => {
 
   return (
     <body className="flex min-h-screen flex-col items-center justify-between p-24">
-    <header className="w-full h-screen bg-[#ebd4c7]">
-        <img class="h-auto max-w-full" src="/PAN_ASIAN.svg" alt="image description">      
-        </img>
+    <header className="w-full h-screen bg-[#ebd4c7]">             
+      <nav className="bg-[#C7DEEB] p-4">
+        <div className="container mx-auto flex items-center justify-between">
+          <div className="text-white text-xl font-bold">Your Logo</div>
+          <ul className="flex items-center space-x-4">
+            <li><a class="mb-3 text-xl font-semibold text-white-700" href="#">Home</a></li>
+            <li><a class="mb-3 text-xl font-semibold text-white-700" href="/aboutpage">About</a></li>
+            <li><a class="mb-3 text-xl font-semibold text-white-700" href="#">Contact</a></li>
+            <li><a class="mb-3 text-xl font-semibold text-white-700" href="#">Your Orders</a></li>
+          </ul>
+        </div>
+      </nav>
+      <img class="h-auto max-w-full" src="/PAN_ASIAN.svg" alt="image description">      
+      </img>
     </header>
-    <div className="mb-10 lg:mb-32 w-full grid text-center">
-      <p>Pan Asian is a place for the clubs involved to celebrate their culture through dance food and more!</p>
-      </div>
+    <div className="mb-10 lg:mb-32 w-full grid text-center text-xl" style={{ color: '#692215' }}>
+     <p>Pan Asian is a place for the clubs involved to celebrate their culture through dance, food, and more!</p>
+    </div>
+
     </body>
 
-    
+  
+);
 
-  );
 };
 
 export default AboutPage;
